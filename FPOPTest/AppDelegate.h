2cb8dc55e0533fa93f98b724b5579f14e3921c93
juxi-ios <1160908321@qq.com>
2015-09-02 10:19:09 +0800

    Initial Commit

---
+++ FPOPTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FPOPTest
+//
+//  Created by juxi-ios on 15/9/2.
+//  Copyright (c) 2015年 ZHOUCAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
